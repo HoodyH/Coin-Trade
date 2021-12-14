5df1f0cab4d6db24adde47c29778dbb5fc8e68ed
HoodyH <smn.not@outlook.com>
2021-12-14 18:24:51 +0100

    update, model and trading script

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from keras.models import Model
-from keras.layers import Dense, Dropout, LSTM, Input, Activation
+from keras.layers import Dense, Dropout, LSTM, Input, Activation, concatenate
 import tensorflow as tf
 import numpy as np
 from predictions.util import csv_to_dataset, history_points
-import matplotlib.pyplot as plt
 
 seed_value = 4
 np.random.seed(seed_value)
@@ -14,60 +13,80 @@ cro = "./datasets/crypto-com-coin_2018-12-14_2021-12-13.csv"
 
 
 def predict():
-    ohlcv_histories, _, next_day_open_values, unscaled_y, y_normaliser = csv_to_dataset(btc)
-    
+    ohlcv_histories, technical_indicators, next_day_open_values, unscaled_y, y_normaliser = csv_to_dataset(btc)
+
     test_split = 0.9
     n = int(ohlcv_histories.shape[0] * test_split)
-    
+
     ohlcv_train = ohlcv_histories[:n]
+    tech_ind_train = technical_indicators[:n]
     y_train = next_day_open_values[:n]
-    
+
     ohlcv_test = ohlcv_histories[n:]
+    tech_ind_test = technical_indicators[n:]
     y_test = next_day_open_values[n:]
-    
+
     unscaled_y_test = unscaled_y[n:]
-    
+
     print(ohlcv_train.shape)
     print(ohlcv_test.shape)
-    
+
     # model architecture
-    
-    lstm_input = Input(shape=(history_points, 5), name='lstm_input')
+
+    # define two sets of inputs
+    lstm_input = Input(shape=(history_points, 4), name='lstm_input')
+    dense_input = Input(shape=(technical_indicators.shape[1],), name='tech_input')
+
+    # the first branch operates on the first input
     x = LSTM(50, name='lstm_0')(lstm_input)
     x = Dropout(0.2, name='lstm_dropout_0')(x)
-    x = Dense(64, name='dense_0')(x)
-    x = Activation('sigmoid', name='sigmoid_0')(x)
-    x = Dense(1, name='dense_1')(x)
-    output = Activation('linear', name='linear_output')(x)
-    
-    model = Model(inputs=lstm_input, outputs=output)
-    adam = tf.optimizers.Adam(lr=0.0005)
+    lstm_branch = Model(inputs=lstm_input, outputs=x)
+
+    # the second branch opreates on the second input
+    y = Dense(20, name='tech_dense_0')(dense_input)
+    y = Activation("relu", name='tech_relu_0')(y)
+    y = Dropout(0.2, name='tech_dropout_0')(y)
+    technical_indicators_branch = Model(inputs=dense_input, outputs=y)
+
+    # combine the output of the two branches
+    combined = concatenate([lstm_branch.output, technical_indicators_branch.output], name='concatenate')
+
+    z = Dense(64, activation="sigmoid", name='dense_pooling')(combined)
+    z = Dense(1, activation="linear", name='dense_out')(z)
+
+    # our model will accept the inputs of the two branches and
+    # then output a single value
+    model = Model(inputs=[lstm_branch.input, technical_indicators_branch.input], outputs=z)
+    adam = tf.optimizers.Adam(learning_rate=0.0005)
     model.compile(optimizer=adam, loss='mse')
-    model.fit(x=ohlcv_train, y=y_train, batch_size=32, epochs=50, shuffle=True, validation_split=0.1)
-    
+    model.fit(x=[ohlcv_train, tech_ind_train], y=y_train, batch_size=32, epochs=50, shuffle=True, validation_split=0.1)
+
     # evaluation
-    y_test_predicted = model.predict(ohlcv_test)
+
+    y_test_predicted = model.predict([ohlcv_test, tech_ind_test])
     y_test_predicted = y_normaliser.inverse_transform(y_test_predicted)
-    y_predicted = model.predict(ohlcv_histories)
+    y_predicted = model.predict([ohlcv_histories, technical_indicators])
     y_predicted = y_normaliser.inverse_transform(y_predicted)
-    
     assert unscaled_y_test.shape == y_test_predicted.shape
     real_mse = np.mean(np.square(unscaled_y_test - y_test_predicted))
     scaled_mse = real_mse / (np.max(unscaled_y_test) - np.min(unscaled_y_test)) * 100
     print(scaled_mse)
-    
+
+    import matplotlib.pyplot as plt
+
     plt.gcf().set_size_inches(22, 15, forward=True)
-    
+
     start = 0
     end = -1
-    
+
     real = plt.plot(unscaled_y_test[start:end], label='real')
     pred = plt.plot(y_test_predicted[start:end], label='predicted')
-    
+
     # real = plt.plot(unscaled_y[start:end], label='real')
     # pred = plt.plot(y_predicted[start:end], label='predicted')
-    
+
     plt.legend(['Real', 'Predicted'])
+
     plt.show()
 
     model.save(f'model.h5')
--- predictions/util.py
+++ predictions/util.py
@@ -1,26 +1,24 @@
 import pandas as pd
 from sklearn import preprocessing
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 history_points = 50
 
 
 def csv_to_dataset(csv_path):
-    data = pd.read_csv(csv_path, usecols=["date", "open", "high", "low", "close", "volume"])
-    data = data.drop('date', axis=1)
-    data = data.drop(0, axis=0)
-
+    data = pd.read_csv(csv_path, usecols=["open", "high", "low", "close"])
     data = data.values
-
-    data_normaliser = preprocessing.MinMaxScaler()
-    data_normalised = data_normaliser.fit_transform(data)
+    
+    normalizer = preprocessing.MinMaxScaler()
+    dataset = normalizer.fit_transform(data)
 
     # using the last {history_points} open close high low volume data points, predict the next open value
     ohlcv_histories_normalised = np.array(
-        [data_normalised[i:i + history_points].copy() for i in range(len(data_normalised) - history_points)]
+        [dataset[i:i + history_points].copy() for i in range(len(dataset) - history_points)]
     )
     next_day_open_values_normalised = np.array(
-        [data_normalised[:, 0][i + history_points].copy() for i in range(len(data_normalised) - history_points)]
+        [dataset[:, 0][i + history_points].copy() for i in range(len(dataset) - history_points)]
     )
     next_day_open_values_normalised = np.expand_dims(next_day_open_values_normalised, -1)
 
